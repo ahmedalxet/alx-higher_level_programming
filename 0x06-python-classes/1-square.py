bd418b67236928493718e29c0c9d9b67cd85a2a6
ahmedalxet <ahmelaq@gmail.com>
2022-12-20 11:05:54 +0300

    2-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, size):
+        self.__size = size
