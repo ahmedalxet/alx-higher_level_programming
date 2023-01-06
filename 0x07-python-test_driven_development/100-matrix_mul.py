e4ef561fffec83304ab6f3ec06a4dd16eb96f962
ahmedalxet <ahmelaq@gmail.com>
2023-01-06 06:34:24 +0300

    correction

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,36 @@
 def matrix_mul(m_a, m_b):
-    # ...
+    """Multiplies two matrices.
+    
+    Args:
+        m_a (list of lists): The first matrix.
+        m_b (list of lists): The second matrix.
+    
+    Returns:
+        list of lists: The result of multiplying m_a and m_b.
+    
+    Raises:
+        TypeError: If m_a or m_b is not a list.
+        TypeError: If m_a or m_b is not a list of lists.
+        ValueError: If m_a or m_b is empty.
+        TypeError: If one element of m_a or m_b is not an integer or a float.
+        TypeError: If m_a or m_b is not a rectangle.
+        ValueError: If m_a and m_b can't be multiplied.
+    """
+    # Check if m_a and m_b are lists
+    if not isinstance(m_a, list) or not isinstance(m_b, list):
+        raise TypeError("m_a must be a list" if not isinstance(m_a, list) else "m_b must be a list")
+    
+    # Check if m_a and m_b are lists of lists
+    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_a must be a list of lists" if not all(isinstance(row, list) for row in m_a) else "m_b must be a list of lists")
+    
+    # Check if m_a and m_b are empty
+    if m_a == [] or m_b == [] or m_a == [[]] or m_b == [[]]:
+        raise ValueError("m_a can't be empty" if m_a == [] or m_a == [[]] else "m_b can't be empty")
+    
+    # Check if one element of m_a or m_b is not an integer or a float
+    if not all(isinstance(elem, (int, float)) for row in m_a for elem in row) or not all(isinstance(elem, (int, float)) for row in m_b for elem in row):
+        raise TypeError("m_a should contain only integers or floats" if not all(isinstance(elem, (int, float)) for row in m_a for elem in row) else "m_b should contain only integers or floats")
     
     # Check if m_a and m_b are rectangles
     row_sizes_a = [len(row) for row in m_a]
