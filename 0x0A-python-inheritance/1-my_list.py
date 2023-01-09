73c96d064c38dca0504cdb10f5dc9e8d678eff5a
ahmedalxet <ahmelaq@gmail.com>
2023-01-09 13:38:42 +0300

    fix 1 my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,16 @@
 class MyList(list):
+    """
+    A subclass of the built-in list type with a added method for printing the list in sorted order.
+    
+    The elements of the list must be of type int.
+    """
+    
     def print_sorted(self):
+        """
+        Print the list in sorted (ascending) order.
+        
+        The list is sorted in place and the original list is not modified.
+        """
         # Create a copy of the list and sort it
         sorted_list = self.copy()
         sorted_list.sort()
