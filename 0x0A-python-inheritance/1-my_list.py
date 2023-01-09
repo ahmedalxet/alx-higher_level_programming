a222c3b61a4ba58863107af37eb7c4e25e8b1c4e
ahmedalxet <ahmelaq@gmail.com>
2023-01-09 13:48:53 +0300

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,18 @@
 class MyList(list):
     """
-    A subclass of the built-in list type with a added method for printing the list in sorted order.
+    A subclass of the built-in list type with a method for printing the list in sorted order.
     
-    The elements of the list must be of type int.
+    This class overrides the default initialization method to allow creating instances of the class without any arguments.
     """
+    
+    def __init__(self):
+        """
+        Initialize the list object.
+        
+        This method overrides the default initialization method of the list class to allow creating instances of the
+        MyList class without any arguments.
+        """
+        super().__init__()
     
     def print_sorted(self):
         """
@@ -11,7 +20,4 @@ class MyList(list):
         
         The list is sorted in place and the original list is not modified.
         """
-        # Create a copy of the list and sort it
-        sorted_list = self.copy()
-        sorted_list.sort()
-        print(sorted_list)
+        print(sorted(self))
