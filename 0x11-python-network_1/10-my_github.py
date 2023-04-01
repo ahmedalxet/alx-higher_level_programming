220e23566f16f046bc0518d9986da6efec513c5e
ahmedalxet <ahmelaq@gmail.com>
2023-04-01 05:35:27 +0300

    9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    response = requests.get('https://api.github.com/user', auth=(username, password))
+
+    try:
+        print(response.json().get('id'))
+    except:
+        print("None")
