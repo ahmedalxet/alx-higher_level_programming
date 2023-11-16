038a34d65796bbe045a2d7426df6ecafb1129a02
Ahmed Abdurehman <ahmelaq@gmail.com>
2023-11-16 20:44:49 +0300

    correction on task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-curl -s $1 | wc -c
+# This script takes a URL, sends a request to that URL using curl
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
