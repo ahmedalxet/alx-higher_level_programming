d80517a78c0c410fdd05b694f0aa1f243daecc37
Ahmed Abdurehman <ahmelaq@gmail.com>
2023-11-24 11:11:41 +0300

    update on task 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL..
-curl -s -X GET "$1" -w "%{http_code}" -o /dev/null | [ "$(curl -s -X GET "$1")" = "200" ] && curl -s -X GET "$1" || echo ""
+# This script takes in a URL, sends a GET request to the 
+# URL using curl, and displays the body of the response 
+# for a status code of 200
+curl -s -X GET -i "$1" | grep -i "HTTP/1.1 200 OK" -A 1000 | sed -n '/^$/,$p'
