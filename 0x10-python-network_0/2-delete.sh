7854f5bc80647fa1294ffe94ede827e10ece1ed9
Ahmed Abdurehman <ahmelaq@gmail.com>
2023-11-24 11:16:28 +0300

    fix on task 3

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# This script sends a DELETE request to the URL passed as the f
+# This script sends a DELETE request to the URL passed as 
+# the first argument using curl and displays the 
+# body of the response
 curl -s -X DELETE "$1"
+
